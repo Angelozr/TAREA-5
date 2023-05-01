e19cf66bc847f55a454dd65918eecf80cba4aad2
Angelozr <angelo-mero123@hotmail.com>
2023-05-01 04:11:18 -0500

    Update validar2.js

--- validar2.js
+++ validar2.js
@@ -1,3 +1,4 @@
+// Expresiones regulares para validar los campos del formulario
 const expresiones_cedula = /^[0-9]{10,10}$/;
 const expresiones_apellido = /^[a-zA-Z]{4,20}$/;
 const expresiones_correo = /^\w+@\w+\.+[aZ-zA]{2,3}$/;
@@ -8,8 +9,11 @@ const expresiones_telefono = /^[0-9]{10,10}$/;
 const expresiones_Área = /^[a-zA-Z ]{5,30}$/;
 const expresiones_sexo = /^[MF]$/;
 
+// Seleccionamos el formulario y añadimos un evento de escucha al enviar el formulario
 const validarRegistro = document.getElementById("formulario-registro");
 validarRegistro.addEventListener("submit", async (e) => {
+
+    // Obtenemos los valores de los campos del formulario
     var cedula = document.getElementById("cedula").value;
     var nombres = document.getElementById("nombres").value;
     var apellidos = document.getElementById("apellidos").value;
@@ -20,47 +24,68 @@ validarRegistro.addEventListener("submit", async (e) => {
     var Área = document.getElementById("Área").value;
     var sexo = document.getElementById("sexo").value;
 
+
+    // Validamos que todos los campos estén llenos
     if (cedula == "" || telefono == "" || nombres == "" || apellidos == "" || correo == "" || password == "" || confirm_password == "" || Área == "" || sexo == "") {
         e.preventDefault();
         alert("Todos los campos son obligatorios");
         return false;
+
+    // Validamos el campo de nombres con expresiones regulares
     } else if (!expresiones_nombre.test(nombres)) {
         e.preventDefault();
         alert("El nombre debe contener entre 4 y 20 caracteres");
         return false;
+
+     // Validamos el campo de cédula con expresiones regulares
     } else if (!expresiones_cedula.test(cedula)) {
         e.preventDefault();
         alert("El campo cèdula solo se aceptan nùmeros y debe tener 10 dígitos");
         return false;
+
+     // Validamos el campo de teléfono con expresiones regulares
     } else if (!expresiones_telefono.test(telefono)) {
         e.preventDefault();
         alert("El campo teléfono solo se aceptan nùmeros y debe tener 10 dígitos");
         return false;
+
+    // Validamos el campo de apellidos con expresiones regulares  
     } else if (!expresiones_apellido.test(apellidos)) {
         e.preventDefault();
         alert("El apellido debe contener entre 4 y 20 caracteres");
         return false;
+
+    // Validamos el campo de correo con expresiones regulares
     } else if (!expresiones_correo.test(correo)) {
         e.preventDefault();
         alert("El correo no es valido");
         return false;
+
+     // Validamos el campo de contraseña con expresiones regulares   
     } else if (!expresiones_contraseña.test(password)) {
         e.preventDefault();
         alert("La contraseña debe contener entre 4 y 12 caracteres");
         return false;
+
+     // Validamos el campo de contraseña con expresiones regulares
     } else if (password != confirm_password) {
         e.preventDefault();
         alert("Las contraseñas no coinciden");
         return false;
+
+    // Validamos el campo del Área  
     } else if (!expresiones_Área.test(Área)) {
         e.preventDefault();
         alert("El área debe contener entre 5 y 30 caracteres");
         return false;
+
     } else if (!expresiones_sexo.test(sexo)) {
         e.preventDefault();
         alert("El sexo debe ser 'M' o 'F'");
         return false;
+
     } else {
+        // Si el formulario es válido, se guarda la información en la base de datos
         const enviar = (cedula, telefono, nombres, apellidos, correo, password, confirm_password, sexo, Área) => {
             db.collection('UsuariosRegistrados').doc().set({
                 cedula,
@@ -85,8 +110,11 @@ validarRegistro.addEventListener("submit", async (e) => {
         const sexo = validarRegistro["sexo"].value;
         const Área = validarRegistro["Área"].value;
 
+
+        // Muestra un mensaje de éxito
         alert("Te has registrado exitosamente");
 
+        // Llama a la función enviar para guardar la información en la base de datos
         await enviar(cedula, telefono, nombres, apellidos, correo, password, confirm_password, sexo, Área);
 
         // AUTENTICACION DE USUARIOS
